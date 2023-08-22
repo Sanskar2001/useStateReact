b1756b0570b4d7fcffeb3d553f3ddc3284419dc1
Sanskar2001 <sanskaratrey22@gmail.com>
2023-08-22 23:54:32 +0530

    added virtual dom

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
 function useState(initialValue) {
-    let state = initialValue;
-    const getState = () => state;
+  let state = initialValue;
+  const getState = () => state;
 
-    const setState = newState => {
-        if (state !== newState) {
-            state = newState;
-            render();
-        }
-    };
+  const setState = (newState) => {
+    if (state !== newState) {
+      state = newState;
+      updateVirtualDOM();
+    }
+  };
 
-    return [getState, setState];
+  return [getState, setState];
 }
 
-const [getText, setText] = useState('Hello World');
-const [getColor,setColor] = useState('red');
+const [getText, setText] = useState("Hello World");
+const [getColor, setColor] = useState("red");
 
-const contentDiv = document.getElementById('contents');
-const button = document.getElementById('btn');
+const contentDiv = document.getElementById("contents");
+const colorBtn = document.getElementById("colorBtn");
+const button = document.getElementById("btn");
 
-function render() {
-    contentDiv.innerText = getText();
-    contentDiv.style.backgroundColor = getColor();
+let prevVirtualDOM = {
+  tag: "div",
+  nodeReference: contentDiv,
+  props: { "style.backgroundColor": "red", innerText: "Hello World" },
+  children: [],
+};
+
+let currentVirtualDOM = {
+  tag: "div",
+  nodeReference: contentDiv,
+  props: { "style.backgroundColor": "red", innerText: "Hello World" },
+  children: [],
+};
+
+const render = () => {
+  console.log(prevVirtualDOM);
+  // console.log(currentVirtualDOM);
+  for (const key in prevVirtualDOM.props) {
+    console.log(key + " : " + prevVirtualDOM.props[key]);
+    console.warn(key + " : " + currentVirtualDOM.props[key]);
+
+    if (prevVirtualDOM.props[key] !== currentVirtualDOM.props[key]) {
+      if (key === "style.backgroundColor") {
+        prevVirtualDOM.nodeReference.style.backgroundColor =
+          currentVirtualDOM.props[key];
+        prevVirtualDOM.props[key] = currentVirtualDOM.props[key];
+      } else {
+        prevVirtualDOM.nodeReference[key] = currentVirtualDOM.props[key];
+        prevVirtualDOM.props[key] = currentVirtualDOM.props[key];
+      }
+    }
+  }
+};
+
+function updateVirtualDOM() {
+  currentVirtualDOM.props["style.backgroundColor"] = getColor();
+  currentVirtualDOM.props.innerText = getText();
+  render();
 }
 
-button.addEventListener('click', () => {
-   getText()==='Hello World' ? setText('Bye World') : setText('Hello World');
-   getColor()==='red' ? setColor('blue') : setColor('red');
-  
+button.addEventListener("click", () => {
+  console.log("clicked");
+  getText() === "Hello World" ? setText("Bye World") : setText("Hello World");
+  getColor() === "red" ? setColor("blue") : setColor("red");
+});
+colorBtn.addEventListener("click", () => {
+  getColor() === "red" ? setColor("blue") : setColor("red");
 });
 
 render();
